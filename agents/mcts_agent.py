919c3a035159e05661fc98c479cdd159bcad89ba
Armand Anusca <ioan.anuscapopa@student.manchester.ac.uk>
2021-11-23 13:22:03 +0000

    Refactored naive agent as base implementation

--- agents/mcts_agent.py
+++ agents/mcts_agent.py
@@ -3,7 +3,7 @@ from random import choice
 from time import sleep
 
 
-class NaiveAgent():
+class MCTSAgent():
     """This class describes the default Hex agent. It will randomly send a
     valid move at each turn, and it will choose to swap with a 50% chance.
     """
@@ -69,40 +69,39 @@ class NaiveAgent():
                 elif s[3] == self.colour:
                     action = [int(x) for x in s[1].split(",")]
                     self.board[action[0]][action[1]] = self.opp_colour()
-
-                    self.make_move()
+                    self.make_move(action)
 
         return False
 
-    def make_move(self):
+    def test_swap(self, action):
+        # TODO: Implement a way to decide if the agent should swap
+        if action:
+            return True
+        return False
+
+    def choose_move(self):
+        choices = []
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if self.board[i][j] == 0:
+                    choices.append((i, j))
+        pos = choice(choices)
+        self.s.sendall(bytes(f"{pos[0]},{pos[1]}\n", "utf-8"))
+        self.board[pos[0]][pos[1]] = self.colour
+
+    def make_move(self, action=None):
         """Makes a random move from the available pool of choices. If it can
         swap, chooses to do so 50% of the time.
         """
 
         # print(f"{self.colour} making move")
         if self.colour == "B" and self.turn_count == 0:
-            if choice([0, 1]) == 1:
+            if self.test_swap(action):
                 self.s.sendall(bytes("SWAP\n", "utf-8"))
             else:
-                # same as below
-                choices = []
-                for i in range(self.board_size):
-                    for j in range(self.board_size):
-                        if self.board[i][j] == 0:
-                            choices.append((i, j))
-                pos = choice(choices)
-                self.s.sendall(bytes(f"{pos[0]},{pos[1]}\n", "utf-8"))
-                self.board[pos[0]][pos[1]] = self.colour
+                self.choose_move()
         else:
-            choices = []
-            for i in range(self.board_size):
-                for j in range(self.board_size):
-                    if self.board[i][j] == 0:
-                        choices.append((i, j))
-            pos = choice(choices)
-
-            self.s.sendall(bytes(f"{pos[0]},{pos[1]}\n", "utf-8"))
-            self.board[pos[0]][pos[1]] = self.colour
+            self.choose_move()
         self.turn_count += 1
 
     def opp_colour(self):
@@ -117,9 +116,6 @@ class NaiveAgent():
             return "None"
 
 
-
-
-
 if (__name__ == "__main__"):
-    agent = NaiveAgent()
+    agent = MCTSAgent()
     agent.run()
